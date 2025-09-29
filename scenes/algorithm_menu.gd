6ec8edf0530357f1779744b6a2b5ae95f8ca8cae
Jenae Matson <jlmatson@alaska.edu>
2025-09-29 13:24:53 -0800

    Added 3 buttons that print a message when pressed

---
+++ scenes/algorithm_menu.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _on_button_1_pressed() -> void:
+	print("Pressed A")
+
+
+func _on_button_2_pressed() -> void:
+	print("Pressed B")
+
+
+func _on_button_3_pressed() -> void:
+	print("Pressed C")
