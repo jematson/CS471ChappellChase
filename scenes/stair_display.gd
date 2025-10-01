ed9311812f1d0f3c07996d1c789ac702592384cd
Cleary <cleary@halfcab.dome>
2025-09-30 18:57:24 -0800

    Added interpolation animation to moving stairs and extended ColorRect to StairRect so each stair node can store info about itself that should integrate cleanly with sorting algorithms later

--- scenes/stair_display.gd
+++ scenes/stair_display.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
+
 var num_stairs = 15
 var stair_heights = []
+var stairs = []
+var indices = []
 @onready var background: ColorRect = $BG
 @onready var bg_position = background.position
 @onready var bg_height = background.size[1]
 @onready var bg_width = background.size[0]
+@onready var bottom_floor = bg_position[1] + bg_height - 50
 
 func _ready() -> void:
 	for i in range(num_stairs):
@@ -17,26 +21,34 @@ func _ready() -> void:
 func draw_stairs() -> void:
 	var stair_width = (bg_width - (10 * (num_stairs+1))) / num_stairs
 	for i in range(num_stairs):
+
 		var stairx = bg_position[0] + i*(stair_width + 10) + 10
 		var stairh = stair_heights[i]*25
-		var stairy = bg_position[1] + bg_height - 50 - stairh
+		var stairy = bottom_floor - stairh
 		
-		var new_node = ColorRect.new()
+		indices.append(Vector3(i,stairx, stairy))
+		var new_node = StairRect.new()
+		new_node.value = i + 1
+		new_node.height = stairh
 		new_node.set_position(Vector2(stairx, stairy))
+		new_node.target_pos = (Vector2(stairx, stairy))
 		new_node.set_size(Vector2(stair_width, stairh))
 		new_node.color = Color.WHITE
 		add_child(new_node)
+		stairs.append(new_node)
 
 
 func shuffle_stairs():
-	var children = get_children()
-	print("Deleting stairs...")
-	if children.size() > 0:
-		for i in range(1,children.size()):
-			var child = children[i]
-			child.queue_free()
-			
-	print("Shuffling stairs...")
-	stair_heights.shuffle()
-	draw_stairs()
+	var new_order = stair_heights.duplicate()
+	new_order.shuffle()
 	
+	for target_index in range(num_stairs):
+		var target_value = new_order[target_index]
+		
+		for stair in stairs:
+			if stair.value == target_value:
+				var target_x = indices[target_index].y
+				var target_y = bottom_floor - stair.height
+				#var target_y = indices[target_index].z
+				stair.target_pos = Vector2(target_x, target_y)
+				break
