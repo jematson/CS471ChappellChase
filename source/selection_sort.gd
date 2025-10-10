b4eb4792b7376629a32b6cbb071b92dd17b79f4f
Cleary <cleary@halfcab.dome>
2025-10-10 12:01:54 -0800

    Linted selection_sort.gd

--- source/selection_sort.gd
+++ source/selection_sort.gd
@@ -2,14 +2,14 @@ extends RefCounted
 
 
 static func selection_sort(stair_heights: Array, swap: Callable, node: Node):
-	var currentMin
-	var minIndex
+	var current_min
+	var min_index
 	for i in range(stair_heights.size()):
-		currentMin = stair_heights[i]
-		minIndex = i
+		current_min = stair_heights[i]
+		min_index = i
 		for j in range(i, stair_heights.size()):
-			if stair_heights[j] < currentMin:
-				currentMin = stair_heights[j]
-				minIndex = j
-		swap.call(stair_heights, minIndex, i)
+			if stair_heights[j] < current_min:
+				current_min = stair_heights[j]
+				min_index = j
+		swap.call(stair_heights, min_index, i)
 		await node.get_tree().create_timer(0.3).timeout
