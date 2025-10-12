9408d6fbe158ac2518b5def364f4642b19eead6a
Jenae Matson <jlmatson@alaska.edu>
2025-10-12 14:02:39 -0800

    Connect function to practice mode button

--- source/main_menu.gd
+++ source/main_menu.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level.tscn")
+
+
+func _on_practice_button_pressed() -> void:
+	print("Practice Mode button pressed")
