fa2d03b96274e3a8fafe494ab8ca0f094af7f2c0
Jenae Matson <jlmatson@alaska.edu>
2025-10-12 14:19:40 -0800

    Change formatting

--- source/level.gd
+++ source/level.gd
@@ -6,7 +6,7 @@ extends Node2D
 # Level Starting Conditions
 var num_stairs = 15
 var stair_start = [1, 3, 2, 4, 5, 7, 6, 8, 9, 11, 13, 10, 12, 15, 14,]
-var algorithm_options = ["BubbleSortButton", "QuickSortButton", "InsertionSortButton"]
+var algorithm_options = ["BubbleSortButton", "QuickSortButton", "InsertionSortButton",]
 
 
 func _ready():
